9bcafff09280e0a1e0c1e573cbf952ab3cb3ad3d
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-07-11 18:18:46 +0600

    decline article modal added and api updated

--- src/Pages/DashBoard/AllArticles/AllArticles.jsx
+++ src/Pages/DashBoard/AllArticles/AllArticles.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState  } from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../Hooks/useAuth";
 import Loading from "../../Shared/Loading/Loading";
 import Swal from 'sweetalert2'
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 
 const AllArticles = () => {
   const axiosSecure = useAxiosSecure();
   const { user } = useAuth();
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedArticle, setSelectedArticle] = useState(null);
+  const [declineReason, setDeclineReason] = useState("");
+
+    const openDeclineModal = (article) => {
+    setSelectedArticle(article);
+    setModalOpen(true);
+    // document.activeElement?.blur(); // ফোকাস সরিয়ে দেয় (warning fix)
+  };
+
+  const closeDeclineModal = () => {
+    setModalOpen(false);
+    setSelectedArticle(null);
+    setDeclineReason("");
+  };
+
   const { data: atricles = [], isLoading,refetch } = useQuery({
     queryKey: ["articles", user?.email],
     queryFn: async () => {
@@ -17,8 +37,8 @@ const AllArticles = () => {
     },
   });
   if (isLoading) return <Loading />;
-  console.log(atricles);
-  //approve button
+  
+  //approve button 
   const handleApprove = async (articleId) => {
   try {
     const res = await axiosSecure.patch(`/articles/${articleId}`, {
@@ -34,6 +54,32 @@ const AllArticles = () => {
     Swal.fire("Error", "Something went wrong", "error");
   }
 };
+
+//DECLINE FUNCI
+  const handleDeclineSubmit = async () => {
+    if (!declineReason.trim()) {
+      alert("Please enter a reason.");
+      return;
+    }
+
+    try {
+      const res = await axiosSecure.patch(`/articles/${selectedArticle._id}`, {
+        status: "declined",
+        declineReason: declineReason,
+      });
+
+      if (res.data.modifiedCount > 0) {
+        alert("Article declined successfully.");
+        closeDeclineModal();
+        refetch(); // নতুন করে data আনো
+      } else {
+        alert("Decline failed.");
+      }
+    } catch (err) {
+      alert("Error occurred during decline.",err);
+    }
+  };
+
   return (
     <div>
       <h2>All Articles</h2>
@@ -71,7 +117,7 @@ const AllArticles = () => {
                 <td className="space-y-3">
                     <div className="flex items-center gap-6">
                         <button onClick={()=>handleApprove(article?._id)} className="btn btn-success btn-sm cursor-pointer">approve</button>
-                        <button className="btn btn-error btn-sm cursor-pointer">decline</button>
+                        <button onClick={() => openDeclineModal(article)}  className="btn btn-error btn-sm cursor-pointer">decline</button>
                     </div>
                     <div className="flex items-center gap-6">
                         <button className="btn btn-error btn-sm cursor-pointer">Delete</button>
@@ -86,7 +132,35 @@ const AllArticles = () => {
             </tbody>
           </table>
         </div>
-      
+    <div>
+         {/* MODAL */}
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={closeDeclineModal}
+        contentLabel="Decline Article"
+        className="p-6 bg-white rounded shadow-lg max-w-md mx-auto mt-20"
+        overlayClassName="fixed inset-0 bg-black/20 bg-opacity-40 flex justify-center items-start"
+      >
+        <h2 className="text-xl font-bold mb-2">
+          Decline: {selectedArticle?.articleTitle}
+        </h2>
+        <textarea
+          rows={4}
+          className="textarea textarea-bordered w-full mb-4"
+          placeholder="Write decline reason..."
+          value={declineReason}
+          onChange={(e) => setDeclineReason(e.target.value)}
+        />
+        <div className="flex justify-end gap-4">
+          <button className="btn" onClick={closeDeclineModal}>
+            Cancel
+          </button>
+          <button className="btn btn-error" onClick={handleDeclineSubmit}>
+            Submit
+          </button>
+        </div>
+      </Modal>
+    </div>
     </div>
   );
 };
